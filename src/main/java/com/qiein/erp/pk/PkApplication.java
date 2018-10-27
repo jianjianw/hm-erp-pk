c6712d82ea77f1e54fa1801705e3c963571f5754
史涛 <st.email@qq.com>
2018-10-27 10:21:48 +0800

    init

---
+++ src/main/java/com/qiein/erp/pk/PkApplication.java
@@ -0,0 +1,18 @@
+package com.qiein.erp.pk;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+//扫描DAO包
+@MapperScan("com.qiein.jupiter.web.dao")
+//扫描实体类包
+@EntityScan("com.qiein.jupiter.web.entity.*")
+public class PkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PkApplication.class, args);
+    }
+}
