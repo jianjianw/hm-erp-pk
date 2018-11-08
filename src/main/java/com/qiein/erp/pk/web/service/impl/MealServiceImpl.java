d222095f838dc47ee8496c579a616915b8cfee0c
HanJF <940522326@qq.com>
2018-11-08 10:01:21 +0800

    1

--- src/main/java/com/qiein/erp/pk/web/service/impl/MealServiceImpl.java
+++ src/main/java/com/qiein/erp/pk/web/service/impl/MealServiceImpl.java
@@ -16,40 +16,61 @@ public class MealServiceImpl implements MealService {
 
     @Autowired
     private MealDao mealDao;
-
+    /**
+     * 新增套餐
+     * @return
+     */
 	@Override
 	public int insertMeal(Meal meal) {
 		return mealDao.insertMeal(meal);
 	}
-
+	/**
+     * 查询所有套餐
+     * @return
+     */
 	@Override
 	public List<Meal> selectAll() {
 		List<Meal> selectAll = mealDao.selectAll();
 		return selectAll;
 	}
-
+	/**
+     * 编辑主套餐
+     * @return
+     */
 	@Override
 	public int updateByMeal(Meal meal) {
 		return mealDao.updateByMeal(meal);
 	}
-
+	/**
+     * 查询套餐类别
+     * @return
+     */
 	@Override
 	public List<DictionaryErp> selectMealType(int companyId) {
 		List<DictionaryErp> mealTypes=mealDao.selectMealType(companyId);
 		return mealTypes;
 	}
-
+	/**
+     * 查询服务ID
+     * @return
+     */
 	@Override
 	public List<ServicePO> selectServiceId(int companyId) {
 		List<ServicePO> servicePo=mealDao.selectServiceId(companyId);
 		return servicePo;
 	}
-
+	/**
+     * 新增套餐类别
+     * @return
+     */
 	@Override
 	public void insertMealType(DictionaryErp dictionaryErp) {
 		mealDao.insertMealType(dictionaryErp);
 	}
-
+	/**
+     * 编辑套餐类别
+     * @return
+     */
 	@Override
 	public void updateMealType(DictionaryErp dictionaryErp) {
 		mealDao.updateMealType(dictionaryErp);
