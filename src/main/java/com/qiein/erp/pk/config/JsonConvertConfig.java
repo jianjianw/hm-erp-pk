80607d4e6442912c9d7cfbe1f5cad275eb8ff4ac
wanglei <13992787306@163.com>
2018-11-08 09:03:34 +0800

    add

---
+++ src/main/java/com/qiein/erp/pk/config/JsonConvertConfig.java
@@ -0,0 +1,40 @@
+package com.qiein.erp.pk.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * 配置json的转换配置
+ */
+@Configuration
+public class JsonConvertConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        //自定义配置...
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        //fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");    // 自定义时间格式
+        fastJsonConfig.setSerializerFeatures(
+                //配置美观的输出格式
+//                SerializerFeature.PrettyFormat,
+                //map输出空值
+//                SerializerFeature.WriteMapNullValue,
+                //空集合输出为[]
+                SerializerFeature.WriteNullListAsEmpty,
+                //将字符串类型字段的空值输出为空字符串 ""
+                SerializerFeature.WriteNullStringAsEmpty,
+                //使用enum的toString方法序列化
+                SerializerFeature.WriteEnumUsingToString,
+                //避免循环引用
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        converter.setFastJsonConfig(fastJsonConfig);
+        converters.add(converter);
+    }
+}
