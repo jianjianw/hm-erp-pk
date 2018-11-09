18dd03848447c0141f1b4bec48b630751d6d388e
HanJF <940522326@qq.com>
2018-11-09 19:31:54 +0800

    1

--- src/main/java/com/qiein/erp/pk/web/controller/MealController.java
+++ src/main/java/com/qiein/erp/pk/web/controller/MealController.java
@@ -74,11 +74,20 @@ public class MealController {
         return ResultInfoUtil.success(servicePo);
     }
     /**
-     * 新增套餐
+     * 新增主套餐
      * @return
      */
     @PostMapping("/insert_meal")
     public ResultInfo insertMeal(@RequestBody Meal meal){
+    		meal.setCompanyId(1);
+    		//查询套餐类别名称
+        	DictionaryErp dictionaryErp=mealService.selecDicName(meal.getMealType(),meal.getCompanyId());
+        	meal.setMealTypeName(dictionaryErp.getDicName());
+        	//查询服务类别名称
+        	ServicePO servicePO=mealService.selectServiceName(meal.getServiceId(),meal.getCompanyId());
+            meal.setServiceName(servicePO.getServiceName());
+            System.out.println(JSONObject.toJSONString(meal));
+            
     		mealService.insertMeal(meal);
         return ResultInfoUtil.success();
     }
@@ -105,8 +114,6 @@ public class MealController {
     	meal.setMealTypeName(dictionaryErp.getDicName());
     	//查询服务类别名称
     	ServicePO servicePO=mealService.selectServiceName(meal.getServiceId(),meal.getCompanyId());
-    	System.out.println(JSONObject.toJSONString(servicePO));
-    	
         meal.setServiceName(servicePO.getServiceName());
         System.out.println(JSONObject.toJSONString(meal));
         
