598c037c34f8e6d650dce0b3dc2551a2a20373c5
wanglei <13992787306@163.com>
2018-11-10 16:26:07 +0800

    add

--- src/main/java/com/qiein/erp/pk/web/dao/SceneScheduleDao.java
+++ src/main/java/com/qiein/erp/pk/web/dao/SceneScheduleDao.java
@@ -1,6 +1,6 @@
 package com.qiein.erp.pk.web.dao;
 
-import com.qiein.erp.pk.web.entity.po.ShootSchedulePO;
+import com.qiein.erp.pk.web.entity.po.SceneSchedulePO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -9,10 +9,10 @@ import java.util.List;
  * 拍摄间档期
  * wang lei 
  */
-public interface ShootScheduleDao {
+public interface SceneScheduleDao {
 
 
-    List<ShootSchedulePO> selectScheduleByDateTime(@Param("companyId") Integer companyId,
+    List<SceneSchedulePO> selectScheduleByDateTime(@Param("companyId") Integer companyId,
                                                    @Param("venueId")Integer venueId,
                                                    @Param("startTime")Integer startTime,
                                                    @Param("endTime")Integer endTime);
