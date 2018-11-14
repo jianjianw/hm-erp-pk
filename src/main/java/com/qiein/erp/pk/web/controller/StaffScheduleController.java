f54172dcdb204c5f026c6d2769a1ea5f5b5f6e0b
HanJF <940522326@qq.com>
2018-11-14 15:09:28 +0800

    1

--- src/main/java/com/qiein/erp/pk/web/controller/StaffScheduleController.java
+++ src/main/java/com/qiein/erp/pk/web/controller/StaffScheduleController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,7 +69,7 @@ public class StaffScheduleController {
      * 人员休息
      * @return
      */
-    @GetMapping("/set_rest")
+    @PostMapping("/set_rest")
     public ResultInfo setRest(@RequestBody StaffScheduleVO staffScheduleVO){
     	System.out.println(JSONObject.toJSONString(staffScheduleVO));
     	 int companyId=1;
