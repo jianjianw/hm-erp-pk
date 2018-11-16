152b7b55f0b1d7c5b6a5a9e6a0b54ea92e3af5d7
HanJF <940522326@qq.com>
2018-11-16 19:54:53 +0800

    1

--- src/main/java/com/qiein/erp/pk/web/controller/StaffScheduleController.java
+++ src/main/java/com/qiein/erp/pk/web/controller/StaffScheduleController.java
@@ -70,7 +70,7 @@ public class StaffScheduleController extends InitController{
 					if(StaffScheduleVOAll.getVenueId().equals(StaffScheduleVORest.getVenueId())&&
 							StaffScheduleVOAll.getStaffId().equals(StaffScheduleVORest.getStaffId())){
 						StaffScheduleVOAll.setId(StaffScheduleVORest.getId());
-						StaffScheduleVOAll.setStatus(StaffScheduleVOAll.getStaffStatus());
+						StaffScheduleVOAll.setStatus(StaffScheduleVORest.getStaffStatus());
 					}
 				}
         	}
