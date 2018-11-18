05a0e36a170036246a4aa088f62711c9c6c2da0f
项良 <871911653@qq.com>
2018-11-18 16:03:56 +0800

    摄像师增加

--- src/main/java/com/qiein/erp/pk/web/entity/vo/ShootRoomVO.java
+++ src/main/java/com/qiein/erp/pk/web/entity/vo/ShootRoomVO.java
@@ -10,6 +10,15 @@ public class ShootRoomVO {
     private Integer start;//开始
     private Integer end;//结束
     private Integer sceneId;//拍摄景id
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
     public Integer getSceneId() {
         return sceneId;
