4e66173516405ce5f924167ae58c05de281dca3b
项良 <871911653@qq.com>
2018-11-19 16:56:31 +0800

    摄像师增加

---
+++ src/main/java/com/qiein/erp/pk/web/entity/vo/OrderLikeVO.java
@@ -0,0 +1,15 @@
+package com.qiein.erp.pk.web.entity.vo;
+
+/**
+ * 订单模糊查询接口
+ */
+public class OrderLikeVO {
+    private Integer id;
+    private String orderNum;
+    private String kzName;
+    private String kzPhone;
+    private String mateName;
+    private String matePhone;
+    private String childName;
+    private Integer type;
+}
